59fc5f0ac07bb10e368e709447fdc67034d44aff
Manuel Sebastian <seferinoubago12@gmail.com>
2022-06-19 03:37:35 -0500

    org2, inicio 1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("que onda people")
+console.log("TESTEO")
+
+var axios = require('axios');
+
+var config = {
+    methot: 'GET',
+    url: 'https://rickandmortyapi.com/api/character/2',
+    headers:{}
+};
+    
+axios(config)
+    .then(function (response){
+    console.log(JSON.stringify(response.data));        
+        })
+    .catch(function (error) {
+    console.error(error);
+        });
+    
